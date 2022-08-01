f022038d6bf473d3157a99f2986321b78ba5673c
eremophobia <succducc0@gmail.com>
2022-08-01 23:54:51 +0200

    crystal-lang l&t

---
+++ src/variables/main.cr
@@ -0,0 +1,13 @@
+var = "Hello Variable!"
+puts var
+
+add1 = 5
+add2 = 5
+puts add1 + add2
+
+sub1 = 10
+sub2 = 5
+puts add1 - add2 # etc . . .
+
+list = ["John", "Mike", "Gordon", "Deez"]
+puts list
